b6c2ac2b6aec85c102f7556db8c90737bf93b834
edgar hernández <edgixx@gmail.com>
2021-10-17 00:14:26 -0500

    Cambios

--- src/components/Signup_Card.jsx
+++ src/components/Signup_Card.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import Container from '@mui/material/Container';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import '../css/signup_card.scss';
+import imgLogo from '../assets/coloredLogo.png';
 const bull = (
     <Box
         component="span"
@@ -18,22 +19,12 @@ const bull = (
 
 function Signup_Card() {
 
-    return(<Card sx={{ minWidth: 275 }}>
-        <CardContent className="Card">  
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-            </Typography>
-            <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-            </Typography>
+    return(
+    <Card variant="outlined" className="Card">
+        <CardContent >  
+        <Container className="Card_logo">
+            <img src={imgLogo} alt="imgLogo"></img>
+        </Container>
         </CardContent>
         
     </Card>
