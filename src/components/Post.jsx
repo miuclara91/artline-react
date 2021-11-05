8b603678ec480ccd0c1319486bc85f1b3519e228
miuclara91 <miuclara91@gmail.com>
2021-11-04 18:59:55 -0600

    Merge branch 'master' of https://github.com/miuclara91/artline-react into ecl-art-react

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,53 @@
-import { Avatar, List, ListItem, ListItemText, ListItemAvatar, Container } from '@mui/material';
-import ImageIcon from '@mui/icons-material/Image';
-import React, { useEffect, useState } from 'react';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Container,
+} from "@mui/material";
+import ImageIcon from "@mui/icons-material/Image";
+import React, { useEffect, useState } from "react";
+import "../css/post.scss";
 
 const Post = (props) => {
-    const { userId } = props;
-    const [post, setPost] = useState([]);
-    console.log(userId);
-    useEffect(() => {
-        obtenerDatos();
-    }, []);
+  const { userId } = props;
+  const [post, setPost] = useState([]);
+  console.log(userId);
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
 
-    const obtenerDatos = async () => {
-        const data = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-        const post = await data.json();
-        setPost(post);
-    }
-
-    return (
-        <Container>
-            {
-                post.map((item) => (
-                    <List >
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <ImageIcon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={item.title} secondary={item.body} />
-                        </ListItem>
-                    </List>
-                ))
-            }
-        </Container>
+  const obtenerDatos = async () => {
+    const data = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
     );
-}
+    const post = await data.json();
+    setPost(post);
+  };
+
+  const dataUser = {
+    foto: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos.jpg",
+  };
+
+  return (
+    <Container>
+      {post.map((item) => (
+        <List>
+          <ListItem>
+            <ListItemAvatar style={{margin: 10}}>
+              <Avatar                
+                  alt="fotoPerfil"
+                  src={dataUser.foto}
+                  sx={{ width: 80, height: 80 }}>              
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={item.title} secondary={item.body} />
+          </ListItem>
+        </List>
+      ))}
+    </Container>
+  );
+};
 
 export default Post;
