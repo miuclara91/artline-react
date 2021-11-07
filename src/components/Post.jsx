9f08fb6557442af9c68fc122ac58612377c7bc58
miuclara91 <miuclara91@gmail.com>
2021-11-07 17:20:09 -0600

    Diseño Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,36 +10,59 @@ import {
   CardMedia,
   CardActions,
   IconButton,
+  Divider
 } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import React, { useEffect, useState } from "react";
+import { useLocalStorage } from "../helpers/useLocalStorage";
+
 
 const Post = (props) => {
   const { userId } = props;
   const [post, setPost] = useState([]);
+  const [nombre, setNombre] = useLocalStorage("nombre", "");
+
   useEffect(() => {
     obtenerDatos();
   }, []);
 
   const obtenerDatos = async () => {
-    const data = await fetch(
-      `https://artline-team10.herokuapp.com/artline/publicaciones/postBYusuario/${userId}`
-    );
-    const post = await data.json();
-    setPost(post);
+    if (userId !== '') {
+      const data = await fetch(
+        `https://artline-team10.herokuapp.com/artline/publicaciones/postBYusuario/${userId}`
+      );
+      const post = await data.json();
+      setPost(post);
+    } else {
+      const data = await fetch(
+        `https://artline-team10.herokuapp.com/artline/publicaciones`
+      );
+      const post = await data.json();
+      setPost(post);
+    }
   };
 
   const dataUser = {
     foto: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos.jpg",
   };
 
+  function verComentario(postId) {
+    // history.push(`/comentarios/${postId}`);
+    // alert('falta mandar a la ruta de comentario por id de publicacion' + postId);
+    console.log(postId);
+  }
+
   return (
     <Container>
       {post.map((item) => (
         <List key={item._id} style={{ margin: 50}}>
-          <ListItem>
+          <ListItem  secondaryAction={
+              <IconButton edge="end" aria-label="comments" color='secondary' onClick={verComentario}>
+            
+              </IconButton>
+            }>
             <ListItemAvatar style={{ margin: 10 }}>
               <Avatar
                 alt="fotoPerfil"
@@ -77,6 +100,7 @@ const Post = (props) => {
               </IconButton>
             </CardActions>
           </Card>
+          <Divider variant="inset" component="li" />
         </List>
       ))}
     </Container>
