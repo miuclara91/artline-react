c70bbe9199ddf1e7fc8b832b36b3235db15ff133
Magda Lucia Hernandez Solis <magdalucia97@gmail.com>
2021-11-28 01:23:58 -0600

    Actualizacion de diseño de Home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { Box, Typography, Container, Button } from "@mui/material";
+import { Box, Typography, Container, Button, Tooltip, IconButton, Fab } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 
 import "../css/home.scss";
-import ImagenInicio from "../assets/home.jpeg";
 import Tema from "../helpers/Tema";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import Post from '../components/Post';
 
@@ -15,24 +15,29 @@ const Home = (props) => {
 
   return (
     <ThemeProvider theme={Tema}>
-      <Container sx={{ display: 'flex' }}>
-        <Box className="header__box">
-          <img src={ImagenInicio} alt="Home" className="header__img" />
-        </Box>
+      <Container className="container">
+        <Box sx={{height: 480, width:1500}} className="header__box"></Box>
         <Box textAlign="center">
-          <Typography variant="h3" color="secondary">
-            Únete a Artline hoy mismo.
-          </Typography>
-          <Button component={Link} to="/Signup" variant="outlined">Sign Up </Button>
-          <Box textAlign="center">
-            <p>¿Ya tienes una cuenta? </p>
+          <Button component={Link} to="/Signup" variant="contained">Sign Up </Button>
+          <Box textAlign="center" mb={1}>
+            <p className='login'>Already have an account? </p>
             <Link to="/login">Log In</Link>
           </Box>
         </Box>
+        <Box textAlign="end">
+        <Fab color="primary" aria-label="Deslice">
+        <Tooltip title={<Typography fontSize={14}>Desliza para ver las publicaciones en artline</Typography>} arrow placement="left" open="true" sx={{fontSize:'18'}}>
+          <IconButton>
+            <ExpandMoreIcon htmlColor="white"/>
+            </IconButton>
+          </Tooltip>
+        </Fab>
+        </Box>
       </Container>
+      
       <Container >
         <Box textAlign="center" mt={4}>
-          <Typography variant="h3" color="primary">
+          <Typography variant="h3" fontFamily="Poppins">
             Lo que está pasando en el mundo artístico
           </Typography>
         </Box>
